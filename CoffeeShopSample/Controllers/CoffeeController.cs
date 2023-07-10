bc86ed9d04367387f8e123008d0c1cfc3d9e6d1a
Chien.do <chien.do@saigontechnology.com>
2023-07-10 12:59:22 +0700

    Update latest code

---
+++ CoffeeShopSample/Controllers/CoffeeController.cs
@@ -0,0 +1,29 @@
+﻿using CoffeeShopSample.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : Controller
+    {
+        private readonly ExampleDataAcessLayer _exampleDataAcessLayer;
+        public CoffeeController(ExampleDataAcessLayer exampleDataAcessLayer)
+        {
+            _exampleDataAcessLayer = exampleDataAcessLayer;
+            
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CoffeeType>> GetCoffeeTypesAsync([FromQuery] string country, CancellationToken cancellationToken )
+        {
+            var coffeeTypes = await _exampleDataAcessLayer.SearchCoffeeTypesByCountryAsync(country, cancellationToken);
+
+            return coffeeTypes;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
